be8cf685f65ffa7d036de4c7132474140dca3369
Steicy Alves do Nascimento <steicy.alves.tradeupgroup@gmail.com>
2019-12-18 15:35:19 -0300

    Implementação da feature upload de arquivos

--- capybara/tests/features/step_definitions/upload_de_arquivos.rb
+++ capybara/tests/features/step_definitions/upload_de_arquivos.rb
@@ -1,5 +1,17 @@
 Quando("eu faço um upload de arquivo") do
     visit('/outros/uploaddearquivos')
 
-    attach_file('upload', '', make_visible: true)
-  end
+    attach_file('upload', '/Users/san/Desktop/Automacao/Capybara/capybara/tests/features/capybara.png', make_visible: true)
+    sleep(5)
+
+    visit('/outros/uploaddearquivos')
+
+    #File.join - Concatena um caminho
+    #Dir - Representa diretórios no sistema de arquivos
+    #.pwd - Retorna o caminho do diretório de trabalho atual
+    @foto = File.join(Dir.pwd, 'features/capybara.png')
+    
+    #Passo o id de um input do tipo file
+    attach_file('upload', @foto, make_visible: true)
+    sleep(5)
+end
