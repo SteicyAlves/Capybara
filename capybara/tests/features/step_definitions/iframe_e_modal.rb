8b16a3e3ce1e3b64cd69d6ce61811f93bbd2c2d7
Steicy Alves do Nascimento <steicy.alves.tradeupgroup@gmail.com>
2019-12-18 09:51:50 -0300

    Implementação da feature iframe e modal

--- capybara/tests/features/step_definitions/iframe_e_modal.rb
+++ capybara/tests/features/step_definitions/iframe_e_modal.rb
@@ -1,7 +1,7 @@
 Quando("entro no iframe e preencho os campos") do
   visit("/mudancadefoco/iframe")
 
-  #Mudo foco para o iframe
+  #Mudo o foco para o iframe
   within_frame("id_do_iframe") do
     #Faço as ações dentro do iframe
 
@@ -14,7 +14,29 @@ end
 
 Quando("entro no modal e verifico o texto") do
   visit("/mudancadefoco/modal")
+  click_on("Abrir")
+
+  #Mudo o foco para o modal
+  within("#modal1") do
+    texto = find("h4")
+    expect(texto.text).to eql "Modal Teste"
+  end
+  sleep(5)
 end
 
 Quando("fecho o modal") do
+  within("#modal1") do
+    find(".modal-close").click
+  end
 end
+
+=begin
+  
+Para mudança de foco temos:
+
+within() - Para modais e outros elementos
+within_frame() - Para frames
+within_fieldset() - Para fieldsets
+within_table() - Para tabelas
+  
+=end
